8f688e9aa38a3a011d6da19e5484bb8a53e2ff03
SYD14316 <soporteydesarrollo@umg.edu.mx>
2025-10-24 13:57:10 -0600

    Reformulacion de proyecto

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+	<?php include 'private/head.php'; ?>
+	<body>
+		<!-- HEADER -->
+      <?php include 'public/header.php'; ?>
+		<!-- NAVIGATION -->
+      <?php include 'public/navigation.php'; ?>
+		<!-- BREADCRUMB -->
+      <?php //include 'template/blank.php'; ?>
+		<!-- SHOP CARDS SECTION -->
+			<?php //include 'template/section_shop_cards.php'; ?>
+		<!-- NEW PRODUCTS SECTION -->
+			<?php //include 'template/section_new_products.php'; ?>
+		<!-- HOT DEAL SECTION -->
+      <?php //include 'template/hot_deal.php'; ?>
+		<!-- TOP SELLING SECTION -->
+			<?php //include 'template/section_top_selling.php'; ?>
+		<!-- SMALL TOP SELLING SECTION -->
+			<?php //include 'template/section_small_top_selling.php'; ?>
+		<!-- NEWSLETTER -->
+			<?php //include 'template/newsletter.php'; ?>
+		<!-- FOOTER -->
+			<?php include 'public/footer.php'; ?>
+		<!-- jQuery Plugins -->
+			<?php include 'private/footer_plugins.php'; ?>
+		<!-- -->
+	</body>
+</html>
---
+++ public/header.php
@@ -0,0 +1,48 @@
+<header>
+  <!-- CONTACTO Y CUENTA -->
+    <?php //include 'public/top_header.php'; ?>
+  <!-- ENCABEZADO PRINCIPAL -->
+    <div id="header">
+      <div class="container">
+        <div class="row">
+          <!-- LOGO -->
+            <div class="col-md-3">
+              <div class="header-logo">
+                <a href="#" class="logo">
+                  <img src="./img/logo.png" alt="">
+                </a>
+              </div>
+            </div>
+          <!-- BARRA DE BUSQUEDA -->
+            <div class="col-md-6">
+              <?php //include 'public/search_bar.php'; ?>
+            </div>
+          <!-- CUENTA -->
+            <div class="col-md-3 clearfix">
+              <div class="header-ctn">
+                <!-- WISHLIST -->
+                  <div>
+                    <a href="#">
+                      <i class="fa fa-heart-o"></i>
+                      <span>Your Wishlist</span>
+                      <div class="qty">2</div>
+                    </a>
+                  </div>
+                <!-- CARRITO -->
+                  <?php include 'public/cart.php'; ?>
+                <!-- Menu Toogle -->
+                  <div class="menu-toggle">
+                    <a href="#">
+                      <i class="fa fa-bars"></i>
+                      <span>Menu</span>
+                    </a>
+                  </div>
+                <!-- -->
+              </div>
+            </div>
+          <!-- -->
+        </div>
+      </div>
+    </div>
+  <!-- -->
+</header>
